af4a9b4b724adb942440ca240bf241e8afd1004a
Slandio Soares <88685339+Lizandro12@users.noreply.github.com>
2023-01-30 15:46:02 +0100

    melhoria do codigo de verificacao das cores

--- app/speetchrecognition.js
+++ app/speetchrecognition.js
@@ -1,5 +1,5 @@
 const elementColors = document.querySelector("#cores");
-
+//INSERÇÃO DE CODIGO DE RECONHECIMENTO DE VOZ
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
@@ -8,18 +8,18 @@ recognition.start();
 
 recognition.addEventListener("result", onSpeak);
 
+//FUNCAO EXECUTADA A CADA VEZ QUE COM O EVENTO DE RECONHECIMENTO DE VOZ
 function onSpeak( event){
     const dataColor = event.results[0][0].transcript;
 
     const color = dataColor.toUpperCase();
-/*     const upperCaseFirstLetter = dataColor[0].toUpperCase();
-    const upperCaseLastLetters = dataColor.langth -1; */
 
     printColors(color);
     verifyColors(color);
 
 };
 
+//FUNCAO QUE IMPRIME NA TELA A COR FALADA
 function printColors(color){
     elementColors.innerHTML = `
     
--- app/validarCor.js
+++ app/validarCor.js
@@ -1,6 +1,8 @@
 
+//ARRAY QUE RECEBE AS CORES FALADAS PARA POSTERIOR VALIDAÇÃO
 const colorForValidate = [];
 
+//ARAY USADO PARA VERIFICAR SE AS CORES FALADAS PERTENSEM AO ARCO IRISS
 const rainbowColors = [
     'VERMELHO',
     'LARANJA',
@@ -11,45 +13,60 @@ const rainbowColors = [
     'VIOLETTA'
 ];
 
+//FUNCAO QUE VERIFICA AS CORES
 function verifyColors(color){
 
+/*     const serachColor = colorForValidate.includes(color)  */
 
-     
-    switch (color){
-        case rainbowColors[0]: 
-            document.body.style.backgroundColor = "red";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[1]:
-            document.body.style.backgroundColor = "orange";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[2]:
-            document.body.style.backgroundColor = "yellow";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[3]:
-            document.body.style.backgroundColor = "green";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[4]:
-            document.body.style.backgroundColor = "blue";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[5]:
-            document.body.style.backgroundColor = "indigo";
-            colorForValidate.push(color);
-            break
-        case rainbowColors[6]:
-            document.body.style.backgroundColor = "violet";
-            colorForValidate.push(color);
-        default: 
-            document.body.style.backgroundColor = "black";
-            document.body.innerHTML = `
-                </h1>ERRO<h1>
-            `;
-            document.body.style.color = "white";
-            document.body.style.fontSize = "4em"
-    };
+        switch (color){
+            case rainbowColors[0]: 
+                if(!colorForValidate.includes(color)){
+                    document.body.style.backgroundColor = "red";
+                    colorForValidate.push(color);
+                }
+                break
+            case rainbowColors[1]:
+                if(!colorForValidate.includes(color)){
+                    document.body.style.backgroundColor = "orange";
+                    colorForValidate.push(color);
+                }
+                break
+            case rainbowColors[2]:
+                if(!colorForValidate.includes(color)){
+                    document.body.style.backgroundColor = "yellow";
+                    colorForValidate.push(color);
+                }
+                break
+            case rainbowColors[3]:
+                if (!colorForValidate.includes(color)) {
+                    document.body.style.backgroundColor = "green";
+                    colorForValidate.push(color);   
+                }
+                break
+            case rainbowColors[4]:
+                if (!colorForValidate.includes(color)) {
+                    document.body.style.backgroundColor = "blue";
+                    colorForValidate.push(color);   
+                }
+                break
+            case rainbowColors[5]:
+                if (!colorForValidate.includes(color)) {
+                    document.body.style.backgroundColor = "indigo";
+                    colorForValidate.push(color);   
+                }
+                break
+            case rainbowColors[6]:
+                if (!colorForValidate.includes(color)) {
+                    document.body.style.backgroundColor = "violet";
+                    colorForValidate.push(color);   
+                }
+            default: 
+                document.body.style.backgroundColor = "black";
+                document.body.innerHTML = `
+                    </h1>ERRO<h1>
+                `;
+                document.body.style.color = "white";
+                document.body.style.fontSize = "4em"
+        };
     console.log(colorForValidate);
 };
